2977b5d53335436127bd60cde371a4d51b0fa9f4
Stefano Roncelli <stefano.roncelli@gmail.com>
2020-04-19 13:23:06 +0200

    upppedated a bit sliding_alignement, moved stuff

--- exercises/sliding_alignment.py
+++ exercises/sliding_alignment.py
@@ -19,43 +19,31 @@
 matrix = {'AA': 2, 'AC': -1, 'AT': -1, 'AG': -2, 'CC': 2, 'CT': 0, 'CG': -1,
           'TT': 2, 'TG': -1, 'GG': 2, 'CA': -1, 'TA': -1, 'GA': -2, 'TC': 0,
           'GC': -1, 'GT': -1, }
-human = open('./data/titin_hu.txt', 'r')
-mouse = open('./data/titin_mo.txt', 'r')
+human = 'AAACCGTCGACACACCGACTCCTACTCAG'
+mouse = 'GACTAGAGCGCCGCGGGGCGGCGCAC'
 seq1 = ''
 seq2 = ''
-for line in human:
-    line = line.rstrip()
-    seq2 += line
-for line in mouse:
-    line = line.rstrip()
-    seq2 += line
+# for line in human:
+#     line = line.rstrip()
+#     seq2 += line
+# for line in mouse:
+#     line = line.rstrip()
+#     seq2 += line
+def who_is_long(seq1, seq2): #Retuns the longest of the two sequences in the form of (short, long, Flag_samesize)
+    len_seq1 = len(seq1)
+    len_seq2 = len(seq2)
+    iters = len_seq1 + len_seq2
+    same_size = False
+    if len_seq1 < len_seq2:
+        return (seq1, seq2, same_size)
+    elif len_seq1 > len_seq2:
+        return (seq2, seq1, same_size)
+    else:
+        same_size = True
+        return (seq2, seq1, same_size)
 
 
-# seq1 = 'TT'
-# seq2 = 'GTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTT'
-len_seq1 = len(seq1)
-len_seq2 = len(seq2)
-iters = len_seq1 + len_seq2
-same_size = False
-if len_seq1 < len_seq2:
-    short = seq1
-    long = seq2
-elif len_seq1 > len_seq2:
-    short = seq2
-    long = seq1
-else:
-    same_size = True
-    short = seq1
-    long = seq2
-len_short = len(short)
-len_long = len(long)
-long = long + '-' * len_short
-short = '-' * len_long + short
-short = list(short)
-long = list(long)
-highest = False
-best_seq1 = ''
-best_seq2 = ''
+
 def score_fun(s1, s2, scoring_matrix):
     score = 0
     gap_penalty = -2
@@ -67,33 +55,45 @@ def score_fun(s1, s2, scoring_matrix):
     print(''.join(s1), ''.join(s2), score, sep = '\n')
     return score
 
-for i in range(iters - 1):
-    score = score_fun(long, short, matrix)
-    if long[-1] == '-' and short[0] == '-':
-         del short[0]
-         del long[-1]
-         score = score_fun(long, short, matrix)
-    elif long[-1] != '-' and short[0] == '-':
-        short.append('-')
-        del short[0]
+def slide(sequence1, sequence2, matrix):
+    short, long, same_size = who_is_long(sequence1, sequence2)
+    len_short = len(short)
+    len_long = len(long)
+    long = long + '-' * len_short
+    short = '-' * len_long + short
+    short = list(short)
+    long = list(long)
+    highest = False
+    best_seq1 = ''
+    best_seq2 = ''
+    iters = len_short + len_short
+    for i in range(iters - 1):
         score = score_fun(long, short, matrix)
-    else:
-        long.insert(0, '-')
-        short.append('-')
-        score = score_fun(long, short, matrix)
-    if highest == False:
-        highest = score
-    if score > highest:
-        best_seq1 = ''.join(long)
-        best_seq2 = ''.join(short)
-        highest = score
-print(highest)
+        if long[-1] == '-' and short[0] == '-':
+            del short[0]
+            del long[-1]
+            score = score_fun(long, short, matrix)
+        elif long[-1] != '-' and short[0] == '-':
+            short.append('-')
+            del short[0]
+            score = score_fun(long, short, matrix)
+        else:
+            long.insert(0, '-')
+            short.append('-')
+            score = score_fun(long, short, matrix)
+        if highest == False:
+            highest = score
+        if score > highest:
+            best_seq1 = ''.join(long)
+            best_seq2 = ''.join(short)
+            highest = score
+    return (best_seq1, best_seq2, highest)
 
-comp = ''
-for base1, base2 in zip(best_seq1, best_seq2):
-    if base1 == base2:
-        comp += '|'
-    else:
-        comp += ' '
-print(best_seq1, comp, best_seq2,sep = '\n')
-print('The best alignment score is:', highest)
+# comp = ''
+# for base1, base2 in zip(best_seq1, best_seq2):
+#     if base1 == base2:
+#         comp += '|'
+#     else:
+#         comp += ' '
+# print(best_seq1, comp, best_seq2,sep = '\n')
+# print('The best alignment score is:', highest)
--- lab/mod2/kunitz/confusion.py
+++ lab/mod2/kunitz/confusion.py
@@ -1,4 +1,4 @@
-import math 
+import math
 
 
 # Training confusion matrix
